4683880327f99e39c958f6c500bce40e9a2c2979
Charlie Ford <charles.ford1978@gmail.com>
2021-09-29 20:12:39 -0400

    Added some comments to the Savings class explaining items in the class.  Still have more to add later.

--- Banking_C33/Savings.cs
+++ Banking_C33/Savings.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Banking_C33
 {
+    
+    //this Savings class demonstrates how to use inheritance functions.  This class inherits the Account class
+    //using the syntax below.  This means that everything that is part of the Account class (all of it's properties
+    //and methods) become a part of this class as well.
     class Savings : Account
     {
+        
+        //since the Account class did not contain a property or method related to interest rate that has to be
+        //created in this class.  This makes sense since the theoretical bank only offers an interest rate
+        //on savings accounts.
+        //Below the InterestRate property is created as a decimal type and setting it a default value of 0.01.
+        //When creating an instance of Savings the user would be able to enter a different value for 'InterestRate'.
         public decimal InterestRate { get; set; } = 0.01m;
 
+        //this creates the CalculateAndPayInterest method which will calculate how much interest would accrue
+        //on the balance of a particular account and deposit the interest amount to the account balance.
+        //The method requires the user to pass in the number of months for each interval in which interest would
+        //calculated and deposited.
         public decimal CalculateAndPayInterest (int months)
         {
             var interest = Balance * (InterestRate / 12) * months;
